ab06a3df0d85a1bfa205ad7e92eab49f2b6c4e18
lakshag32 <lakshag32@gmail.com>
2024-05-23 20:36:17 -0700

    combine all data into one csv and get all headlines

--- sentiment_analyzer copy.py
+++ sentiment_analyzer copy.py
@@ -5,77 +5,62 @@ import ast
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-nvidia_finance_titles = []
-nvidia_stock_titles = []
 nvidia_titles = []
-
-nvidia_finance_desc = []
-nvidia_stock_desc = []
 nvidia_desc = []
 
-nvidia_df = pd.read_csv("data//nvidia.csv")
-nvidia_stock_df = pd.read_csv("data//nvidia_stock.csv")
-nvidia_finance_df = pd.read_csv("data//nvidia_finance.csv")
+AU_nvidia = pd.read_csv('data//Australia_nvidia.csv', on_bad_lines='warn')
+AU_nvidia_finance = pd.read_csv('data//Australia_nvidia finance.csv', on_bad_lines='warn')
+AU_nvidia_stock = pd.read_csv('data//Australia_nvidia stock.csv', on_bad_lines='warn')
 
-for row in range(len(nvidia_df)): 
-    row_data = ast.literal_eval(nvidia_df["content.results.main"][row])
+US_nvidia = pd.read_csv('data//United States_nvidia.csv', on_bad_lines='warn')
+US_nvidia_finance = pd.read_csv('data//United States_nvidia.csv', on_bad_lines='warn')
+US_nvidia_stock = pd.read_csv('data//United States_nvidia.csv', on_bad_lines='warn')
+
+UK_nvidia = pd.read_csv('data//United Kingdom_nvidia.csv', on_bad_lines='warn')
+UK_nvidia_finance = pd.read_csv('data//United Kingdom_nvidia.csv', on_bad_lines='warn')
+UK_nvidia_stock = pd.read_csv('data//United Kingdom_nvidia.csv', on_bad_lines='warn')
+
+IN_nvidia = pd.read_csv('data//India_nvidia.csv', on_bad_lines='warn')
+IN_nvidia_finance = pd.read_csv('data//India_nvidia.csv', on_bad_lines='warn')
+IN_nvidia_stock = pd.read_csv('data//India_nvidia.csv', on_bad_lines='warn')
+
+all_data = pd.concat([AU_nvidia, AU_nvidia_finance, AU_nvidia_stock,US_nvidia,US_nvidia_finance,US_nvidia_stock,UK_nvidia,UK_nvidia_finance,UK_nvidia_stock,IN_nvidia,IN_nvidia_finance,IN_nvidia_stock], ignore_index=True, axis=0)
+
+for row in range(len(all_data)): 
+    row_data = ast.literal_eval(all_data["content.results.main"][row])
     for site in row_data: 
         if "hour" in site["relative_publish_date"]: 
             nvidia_titles.append(site['title'])
-            nvidia_desc.append(site["desc"])
         elif "minute" in site["relative_publish_date"]: 
             nvidia_titles.append(site['title'])
-            nvidia_desc.append(site["desc"])
 
-for row in range(len(nvidia_stock_df)): 
-    row_data = ast.literal_eval(nvidia_stock_df["content.results.main"][row])
-    for site in row_data: 
-        if "hour" in site["relative_publish_date"]: 
-            nvidia_stock_titles.append(site['title'])
-            nvidia_stock_desc.append(site["desc"])
+all_article_titles = nvidia_titles
+print(len(list(set(all_article_titles))))
 
-        elif "minute" in site["relative_publish_date"]: 
-            nvidia_stock_titles.append(site['title'])
-            nvidia_stock_desc.append(site["desc"])
+# tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+# model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
-for row in range(len(nvidia_finance_df)): 
-    row_data = ast.literal_eval(nvidia_finance_df["content.results.main"][row])
-    for site in row_data: 
-        if "hour" in site["relative_publish_date"]: 
-            nvidia_finance_titles.append(site['title'])
-            nvidia_finance_desc.append(site["desc"])
-
-        elif "minute" in site["relative_publish_date"]: 
-            nvidia_finance_titles.append(site['title'])
-            nvidia_finance_desc.append(site["desc"])
-
-all_article_titles = nvidia_titles + nvidia_finance_titles + nvidia_stock_titles
-all_article_descs = nvidia_desc + nvidia_finance_desc + nvidia_stock_desc
-
-tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-
-total_headline_sentiment = 0 
-total_desc_sentiment = 0 
+# total_headline_sentiment = 0 
+# total_desc_sentiment = 0 
 
 
-for headline in all_article_titles:
-    tokens = tokenizer.encode(headline, return_tensors='pt') #return pytorch tensor/s(?) from sentiment model output
-    model_output_tensor = model(tokens)
-    sentiment_rating = int(torch.argmax(model_output_tensor.logits))+1
-    total_headline_sentiment += sentiment_rating
-
-# for desc in all_article_descs:
-#     tokens = tokenizer.encode(desc, return_tensors='pt') #return pytorch tensor/s(?) from sentiment model output
+# for headline in all_article_titles:
+#     tokens = tokenizer.encode(headline, return_tensors='pt') #return pytorch tensor/s(?) from sentiment model output
 #     model_output_tensor = model(tokens)
 #     sentiment_rating = int(torch.argmax(model_output_tensor.logits))+1
-#     total_desc_sentiment += sentiment_rating
+#     total_headline_sentiment += sentiment_rating
 
-avg_NVIDIA_headline_sentiment = total_headline_sentiment/len(all_article_titles)
-# avg_NVIDIA_desc_sentiment = total_desc_sentiment/len(all_article_titles)
+# # for desc in all_article_descs:
+# #     tokens = tokenizer.encode(desc, return_tensors='pt') #return pytorch tensor/s(?) from sentiment model output
+# #     model_output_tensor = model(tokens)
+# #     sentiment_rating = int(torch.argmax(model_output_tensor.logits))+1
+# #     total_desc_sentiment += sentiment_rating
 
-print(avg_NVIDIA_headline_sentiment)
-# print(avg_NVIDIA_desc_sentiment)
+# avg_NVIDIA_headline_sentiment = total_headline_sentiment/len(all_article_titles)
+# # avg_NVIDIA_desc_sentiment = total_desc_sentiment/len(all_article_titles)
+
+# print(avg_NVIDIA_headline_sentiment)
+# # print(avg_NVIDIA_desc_sentiment)
 
 
 
