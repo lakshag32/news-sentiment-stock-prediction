e7d4f62e6b99361e331f2c883f3f0c90bb0caf98
lakshag32 <lakshag32@gmail.com>
2024-05-23 17:39:37 -0700

    add more ideas, begin making a more iterative apporach to search result collection

---
+++ oxy_labs_scraper.py
@@ -0,0 +1,57 @@
+#https://oxylabs.io/blog/how-to-scrape-google-search-results?utm_source=youtube&utm_medium=organic_video&utm_content=How%20to%20Scrape%20Google%20Search%20Results:%20A%20Step-by-Step%20Guide?utm_source=youtube&utm_medium=organic_video&utm_content=How%20to%20Scrape%20Google%20Search%20Results:%20Python%20Tutorial
+#https://oxylabs.io
+
+import requests
+from pprint import pprint
+import pandas as pd
+
+# payload = {
+#     'source': 'google_search',
+#     'query': 'shoes',
+#     'domain': 'de',
+#     'geo_location': 'Germany',
+#     'locale': 'en-us',
+#     'parse': True,
+#     'start_page': 1,
+#     'pages': 5,
+#     'limit': 10,
+# }
+
+queries = ['nvidia','nvidia stock','nvidia finance']
+countries = ["United States",'Australia',"United Kingdom", "India"]
+
+for country in countries: 
+    for query in queries: 
+        payload = {
+            "source": "google_search",
+            "query": query,
+            "parse": True,
+            "context": [
+                {
+                    "key": "tbm",
+                    "value": "nws"
+                }
+            ],
+            'geo_location': country,
+            'locale': 'en-us',
+            "pages": 14,
+            "limit": 10,
+        }
+
+        # Get response.
+        response = requests.request(
+            'POST',
+            'https://realtime.oxylabs.io/v1/queries',
+            auth=('LorenzoThePasta', '=362f5JPNFahs_Y'),
+            json=payload,
+        )
+
+        if response.status_code != 200:
+            print("Error - ", response.json())
+            exit(-1)
+
+        pprint(response.json())
+
+        data = response.json()
+        df = pd.json_normalize(data['results'])
+        df.to_csv('data.csv', index=False)
