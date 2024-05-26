8d903135589e9013ec1f8208956bfc4b69b77eda
lakshag32 <lakshag32@gmail.com>
2024-05-25 23:55:37 -0700

    iteratively search through pages of google search queries for each company, save results to csvs for each

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,24 +1,41 @@
 #https://www.youtube.com/watch?v=NB8OceGZGjA&t=1569s
 #https://www.youtube.com/watch?v=ztbFY_kL4jI&t=898s
 #https://www.selenium.dev/documentation/
+#https://stackoverflow.com/questions/52876136/google-search-next-pages-using-selenium
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+import pandas as pd
+from tqdm import tqdm
+
+companies = ["nvidia","tesla",'apple','amd','alibaba','google','microsoft']
+countries =[] 
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-companies = [] 
+for company in tqdm(companies): 
+    queries = [f'{company}',f'{company}+yahoo',f'products+{company}',f'{company}+company', f'{company}+buy+or+sell', f'{company}+bad+or+good', f'{company}+ai', f'{company}+report', f'{company}+Q1',f'{company}+stock+split',f'{company}+finance']
+    headlines = []
 
-driver.get("https://www.google.com/search?q=nvidia&sca_esv=8d9cbe235051cbdc&tbm=nws&prmd=nsivmbt&source=lnt&tbs=qdr:d&sa=X&ved=2ahUKEwif4JaTxKqGAxUVADQIHdNpAVkQpwV6BAgBEAg&biw=1232&bih=616&dpr=1.5")
-driver.implicitly_wait(2)
+    for query in queries: 
+        driver.get(f"https://www.google.com/search?q={query}&sca_esv=8d9cbe235051cbdc&tbm=nws&prmd=nsivmbt&source=lnt&tbs=qdr:d&sa=X&ved=2ahUKEwif4JaTxKqGAxUVADQIHdNpAVkQpwV6BAgBEAg&biw=1232&bih=616&dpr=1.5")
+        
+        
+        while True: 
+            time.sleep(2) #change to 4 or greater for actual
+            titles = driver.find_elements(By.XPATH,'//div[@class="n0jPhd ynAwRc MBeuO nDgy9d"]')
 
-input_elements = driver.find_elements(By.XPATH,'//div[@class="n0jPhd ynAwRc MBeuO nDgy9d"]')
+            for title in titles: 
+                headlines.append(title.text)
 
-print(len(input_elements))
-for element in input_elements: 
-    print(element.text)
+            try:
+                driver.find_element(By.XPATH,"//*[contains(local-name(), 'span') and contains(text(), 'Next')]").click()
+            except:
+                break
+
+    df = pd.DataFrame(headlines)
+    df.to_csv(f"{company}_headlines.csv", index=False)
+    
